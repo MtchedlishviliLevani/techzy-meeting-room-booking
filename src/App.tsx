ddc652804362fac9c33800e76ce51ef8e8421643
Levan Mtchedlishvili <levan.mtchedlishvili.5@iliauni.edu.ge>
2026-08-30 01:39:21 +0400

    feat: build bookings page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router";
+import { BookingsProvider } from "@/context";
 import { Header } from "./components";
 
 function App() {
   return (
-    <div className="min-h-screen px-3 py-3 sm:px-6 sm:py-5 lg:px-8 lg:py-6">
-      <Header />
-      <main className="mt-4 sm:mt-6">
-        <Outlet />
-      </main>
-    </div>
+    <BookingsProvider>
+      <div className="min-h-screen px-3 py-3 sm:px-6 sm:py-5 lg:px-8 lg:py-6">
+        <Header />
+        <main className="mt-4 sm:mt-6">
+          <Outlet />
+        </main>
+      </div>
+    </BookingsProvider>
   );
 }
 
